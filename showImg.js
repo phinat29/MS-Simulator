45b595f7d4c784d9327d0879ab5ca350b47bdb39
MoutOgm <philnat29@orange.fr>
2020-06-24 19:10:00 +0200

    Next

--- showImg.js
+++ showImg.js
@@ -2,7 +2,6 @@ var fs = require('fs')
 var fileIF = fs.readFileSync("IF.json")
 var IF = JSON.parse(fileIF)
 var Skills = require("./FightFiles/Skill").Skill
-
 var folder = "MSSprites"
 var fScript = "MScripts"
 var monster
@@ -14,15 +13,16 @@ var MonsterSelected = {Left: [null, null, null], Right: [null, null, null]}
 function loadImgM() {
     var files = fs.readdirSync(fScript)
     for  (var i in files) {
-        document.getElementsByClassName("img")[0].innerHTML += "<input type=\"image\" id=\""+ files[i].slice(0, -5) +"\" class=\"spriteM\" src=\"" + folder + "/" + files[i].slice(0, -5) + ".png\" onmouseup=\"selectImgM(event)\">"
+        document.getElementsByClassName("img")[0].innerHTML += "<div class=\"containerI\" onmouseup=\"selectImgM(this, event)\"><img id=\""+ files[i].slice(0, -5) +"\" class=\"spriteM\" src=\"" + folder + "/" + files[i].slice(0, -5) + ".png\"></div>"
     }
 }
-function selectImgM(e) {
+function selectImgM(s, e) {
     if (e.which == 3) {
-        var id = e.currentTarget.id
+        var id = s.children[0].id
         if (monster) { if (id == monster.id) { return ; } }
         monster = fs.readFileSync(fScript + "/" + id + ".json")
         monster = JSON.parse(monster)
+        monster.points = monster.RStats.lvl +1
         var info = document.getElementsByClassName("info")[0]
         info.innerHTML = infoDiv()
         document.getElementsByClassName("IF")[0].style.visibility = "visible"
@@ -74,7 +74,7 @@ function infoDiv() {
     }
 
     var info = "<img id=\"" + monster.id + "\"class=\"spriteM zoom showInfo\" src=\"" + folder + "/" + monster.id + ".png" + "\">"+ monster.id
-    info += "<input class=\"MLevel levelInfo\" type=\"number\" min=\"1\" max=\"36\" placeholder=\"level\" value=\"" + monster.RStats.lvl + "\">"
+    info += "<input class=\"MLevel levelInfo\" id=\"LevelMonster\" type=\"number\" min=\"1\" max=\"36\" placeholder=\"level\" value=\"" + monster.RStats.lvl + "\" oninput=\"lvlChanged(this)\">"
     info += "<div class=\"buttonInfo\"><button onclick=\"buttonClick(event)\" id=\"calcul\" class=\"buttonsI bCalcul\">Calcul</button><button onclick=\"buttonClick(event)\" id=\"transfer\" class=\"buttonsI bTransfer\">Transfer</button><button onclick=\"buttonClick(event)\" id=\"reset\" class=\"buttonsI bReset\">Reset</button></div>"
     info += "<div class=\"statsInfo\">"
     info += "<div id=\"statsName\">Stats : </div><div class=\"StatsListRating\">Attack rating : "+ monster.RStats.ADR +"<br>Magic Rating : "+ monster.RStats.APR +"<br>Defense Rating : "+ monster.RStats.DR +"<br>HP Rating : "+ monster.RStats.HPR +"<br>Mana Rating : "+ monster.RStats.MaR +"</div>"
@@ -92,7 +92,7 @@ function infoDiv() {
     info  += "Crit Chance : "+ (monster.RStats.CC+ monster.baseStats? stats.CC : 0) *100 +"%<br>Crit Damage : "+ (monster.RStats.CD+ monster.baseStats? stats.CD : 0) *100 +"%<br>Damage Reduc : "+ Math.trunc(monster.baseStats.reduc *100) +"%"
     info += "</div></div>"
     var gridtc = ""
-    for (var i = 0; i < monster.treeBool.length; i++) { gridtc += "1fr " }
+    for (var i = 0; i < monster.treeDes.length; i++) { gridtc += "1fr " }
     info += "<div class=\"treeSkill\" style=\"grid-template-columns: "+gridtc+ ";\">"
     for (var u in monster.treeDes) {
         info += "<div class=\"showSkill\" style=\"grid-column:" + (parseFloat(u) + 1) +"\">"
@@ -106,7 +106,7 @@ function infoDiv() {
                 }else{
                     toShow = monster.treeDes[u][i][k]
                 }
-                info += "<img class=\"spritesSkill\" onclick=\"AddSkill(this, '"+toShow+"', "+lastchar+")\" onmouseover=\"showInfoSkill('"+toShow+"', "+lastchar+")\" src=\"SkillSprites/"+toShow+".png\">"
+                info += "<img class=\"spritesSkill\" onclick=\"AddSkill(this, '"+toShow+"', "+lastchar+", "+i+")\" onmouseenter=\"showInfoSkill('"+toShow+"', "+lastchar+")\" onmouseleave=\"unshowInfo()\" src=\"SkillSprites/"+toShow+".png\">"
             }
             info += "<br>"
         }
@@ -236,25 +236,38 @@ function showUpgrade(i, ID) {
     }
     return a
 }
+function lvlChanged(s) {
+    monster.RStats.lvl = s.value
+    monster.points = monster.RStats.lvl +1
+}
 function resetItems() {
     Food = []
     Items = []
-
     for (var i in document.getElementsByClassName('setToNull')) {
         document.getElementsByClassName('setToNull')[i].value = ""
     }
+    monster.points = monster.RStats.lvl +1
 }
 function showInfoSkill(stringi, up) {
     var infoS = document.getElementsByClassName('infoSkill')[0]
+    var lvl = document.getElementById('LevelMonster')
     infoS.innerHTML = ""
     infoS.style.visibility = "visible"
-    console.log(up)
     if (up >= 0) {
         infoS.innerHTML += stringi
     }
 }
-function AddSkill(s, stringi, up) {
-    s.style.filter = "brightness(1)"
+function unshowInfo() {
+    document.getElementsByClassName('infoSkill')[0].style.visibility = "hidden"
+}
+function AddSkill(s, stringi, up, i) {
+    var lvlRegister = i==0||i==1? 1 : (i-1)*10
+    if (monster.points > 0 && lvlRegister <= monster.RStats.lvl) {
+        if (true) {
+            s.style.filter = "brightness(1)"
+            monster.points--
+        }
+    }
 }
 function StartFight() {
     const remote = require('electron').remote;
@@ -265,4 +278,4 @@ function StartFight() {
     });
     win.setMenuBarVisibility(false)
     win.loadURL('file://FightFiles/index.html');
-  }
+}
